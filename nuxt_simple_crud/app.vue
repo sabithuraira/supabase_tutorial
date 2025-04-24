cc75177f851493037dfb1c5b76fdfb584d13031e
Sabit Huraira <sabitzhabit@gmail.com>
2025-04-24 20:17:03 +0700

    simple crud

--- nuxt_simple_crud/app.vue
+++ nuxt_simple_crud/app.vue
@@ -1,6 +1,64 @@
+<script setup>
+  import { createClient } from '@supabase/supabase-js'
+  const supabase = createClient('your_url', 
+    'your anon key')
+  
+  const datas = ref([])
+  const form = ref({ id: "", name: "", desc: ""}) 
+
+  //get data
+  async function getDatas() {
+    const { data } = await supabase.from('animals').select()
+    datas.value = data
+  }
+
+  function formUpdate(id, name, desc){
+    form.value = { id: id, name: name, desc: desc}
+  }
+
+  async function saveData(id, name, desc){
+    if(id==""){
+        const { data, error } = await supabase.from('animals')
+            .insert({ name: name, description: desc })
+            .select()
+        datas.value.push(data[0])
+    }
+    else{
+        const { error } = await supabase.from('animals')
+            .update({ name: name, description: desc })
+            .eq('id', id)
+        getDatas()
+    }
+
+    form.value = { id: "", name: "", desc: ""}
+  }
+
+  async function deleteData(id){
+    const response = await supabase.from('animals')
+          .delete()
+          .eq('id', id)
+
+    getDatas()
+  }
+
+  onMounted(() => {
+    form.value = { id: "", name: "", desc: ""}
+    getDatas()
+  })
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <ul>
+      <li v-for="data in datas" :key="data.id">
+        <a href="#" @click="formUpdate(data.id, data.name, data.description)">Edit</a> | 
+        <a href="#" @click="deleteData(data.id)">Delete</a>  
+        {{ data.name }} - {{ data.description }}
+      </li>
+    </ul>
+
+    Name : <input type="text" v-model="form.name" /> <br/>
+    Desc : <input type="text" v-model="form.desc" /> <br/>
+    <button @click="saveData(form.id, form.name , form.desc)">Save Data</button>
   </div>
 </template>
