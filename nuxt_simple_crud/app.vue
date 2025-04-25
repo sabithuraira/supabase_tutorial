9098d7b84a55ac0f55317787e6894a7db08020b0
sabithuraira <sabitzhabit@gmail.com>
2025-04-25 17:26:15 +0700

    crud with relation

--- nuxt_simple_crud/app.vue
+++ nuxt_simple_crud/app.vue
@@ -6,28 +6,39 @@
   const supabase = useSupabaseClient()
 
   const datas = ref([])
-  const form = ref({ id: "", name: "", desc: ""}) 
+  const form = ref({ id: "", name: "", desc: "", category_id: ""}) 
+  const categories =  ref([])
 
   //get data
   async function getDatas() {
-    const { data } = await supabase.from('animals').select()
+    const { data } = await supabase.from('animals')
+        .select(`*, categories(id, name)`)
     datas.value = data
+    getCategories()
+  }
+  
+  //get list of categories
+  async function getCategories() {
+    const { data } = await supabase.from('categories').select()
+    categories.value = data
   }
 
-  function formUpdate(id, name, desc){
-    form.value = { id: id, name: name, desc: desc}
+  function formUpdate(id, name, desc, category_id){
+    form.value = { id: id, name: name, desc: desc, category_id: category_id}
   }
 
-  async function saveData(id, name, desc){
+  async function saveData(id, name, desc, category_id){
+    let form_object = { name: name, description: desc, category_id: category_id }
+
     if(id==""){
         const { data, error } = await supabase.from('animals')
-            .insert({ name: name, description: desc })
-            .select()
+            .insert(form_object)
+            .select(`*, categories(id, name)`)
         datas.value.push(data[0])
     }
     else{
         const { error } = await supabase.from('animals')
-            .update({ name: name, description: desc })
+            .update(form_object)
             .eq('id', id)
         getDatas()
     }
@@ -53,14 +64,20 @@
   <div>
     <ul>
       <li v-for="data in datas" :key="data.id">
-        <a href="#" @click="formUpdate(data.id, data.name, data.description)">Edit</a> | 
+        <a href="#" @click="formUpdate(data.id, data.name, data.description, data.category_id)">Edit</a> | 
         <a href="#" @click="deleteData(data.id)">Delete</a>  
-        {{ data.name }} - {{ data.description }}
+        {{ data.name }} - {{ data.description }}  {{ data.category_id==null ? "" : " - " + data.categories.name }}
       </li>
     </ul>
 
     Name : <input type="text" v-model="form.name" /> <br/>
     Desc : <input type="text" v-model="form.desc" /> <br/>
-    <button @click="saveData(form.id, form.name , form.desc)">Save Data</button>
+    Categories : 
+      <select v-model="form.category_id">
+        <option v-for="c in categories" :key="c.id" :value="c.id" :selected="form.category_id==c.id ? 'selected' : ''">
+          {{ c.name }}
+        </option>
+      </select><br/>
+    <button @click="saveData(form.id, form.name , form.desc, form.category_id)">Save Data</button>
   </div>
 </template>
